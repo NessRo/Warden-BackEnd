5b39a39a90c610c4aa0f06d32dbf9f6250c16c1c
ness <nrostane@gmail.com>
2023-05-18 16:14:42 -0400

    changed backend api for append questionaires

--- app/models/admin.py
+++ app/models/admin.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import MetaData
 from extensions import db
+from datetime import datetime
+
 
 
 
@@ -20,26 +22,88 @@ def get_questionaire_templates():
 
 def append_questionaire_templates(data):
 
-    sections = data.get('sections')
+    #Extract Template Master Data
+    Template_Data = {
+        'tenant_id':0,
+        'questionaire_template_id':data.get('id'),
+        'questionaire_name':data.get('TemplateName'),
+        'created_by':'Nassim Rostane',
+        'updated_by':'Nassim Rostane',
+        'status':'Published'
+    }
+
+    #Extract Sections
+    sections = data.get('Sections')
+    section_data = []
+
+    for index, section in enumerate(sections):
+        
+        section_dict = {
+        'questionaire_template_id': data.get('id'),
+        'section_id': section.get('id'),
+        'section_name': section.get('title'),
+        'section_index': index,
+        }
+
+        section_data.append(section_dict)
+
+    #Extract Questions
     questions = []
+    questions_data = []
     for section in sections:
-        questions.extend(section.get('questions'))
-        del section['questions']
+        questions.extend(section.get('questions',[]))
 
+    for index, question in enumerate(questions):
 
-    del data['sections']
+        question_dict = {
+            'section_id':question.get('section_id'),
+            'question_id':question.get('id'),
+            'question_type':question.get('type'),
+            'question_index': index,
+            'question_content':question.get('content'),
+            'required':question.get('required'),
+            'logical':question.get('logical'),
+            'condition_value':question.get('conditional_value',None),
+            'condition_type':question.get('condition_type',None),
+            'parent_id':question.get('question_id',None)
+        }
+
+        questions_data.append(question_dict)
+
+    #Extract Sub-Questions
+    for question in questions:
+        if question.get('logical',False) == True:
+            conditional_question = question.get('conditional_question')
+            if conditional_question is not None:
+                conditional_sub_question = conditional_question.get('condition_sub_question')
+                if conditional_sub_question is not None:
+                    question_dict = {
+                    'section_id':conditional_sub_question.get('section_id'),
+                    'question_id':conditional_sub_question.get('id'),
+                    'question_type':conditional_sub_question.get('type'),
+                    'question_index': None,
+                    'question_content':conditional_sub_question.get('content'),
+                    'required':conditional_sub_question.get('required'),
+                    'logical':conditional_sub_question.get('logical'),
+                    'condition_value':conditional_question.get('condition_value',None),
+                    'condition_type':conditional_question.get('condition_type',None),
+                    'parent_id':conditional_sub_question.get('question_id',None)
+                    }
+
+                    questions_data.append(question_dict)
+                
 
     # Create a new instance of the AdminQuestionaireTemplate model with the provided data
-    new_template = AdminQuestionaireTemplate(**data)
+    new_template = AdminQuestionaireTemplate(**Template_Data)
     db.session.add(new_template)
 
     # Loop through and Create a new instance of the AdminQuestionaireSectionTemplate model with the provided data
-    for section in sections:
+    for section in section_data:
         new_section = AdminQuestionaireSectionTemplate(**section)
         db.session.add(new_section)
 
     # Loop through and Create a new instance of the  AdminQuestionaireSectionQuestionsTemplate model with the provided data
-    for question in questions:
+    for question in questions_data:
         new_question = AdminQuestionaireSectionQuestionsTemplate(**question)
         db.session.add(new_question)
 
